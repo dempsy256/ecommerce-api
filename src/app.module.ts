d0ca60355887fa8005f21cd45248d806405c423a
Redemptor <namiremberedemptor@gmail.com>
2026-04-17 06:59:47 +0300

    feat: scaffold REST API endpoints and services for all entities

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
+import { ClientsModule } from './clients/clients.module';
+import { VendorsModule } from './vendors/vendors.module';
+import { ProductsModule } from './products/products.module';
+import { OrdersModule } from './orders/orders.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [PrismaModule, ClientsModule, VendorsModule, ProductsModule, OrdersModule],
   controllers: [AppController],
   providers: [AppService],
 })
